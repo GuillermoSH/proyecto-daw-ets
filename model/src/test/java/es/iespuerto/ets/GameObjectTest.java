409d1998842aa9e89e1ff050f98b9f947fdd88b3
jonapana <jona_pana13@hotmail.com>
2022-06-02 20:10:51 +0100

    se quitan imports innecesarios de los test

--- model/src/test/java/es/iespuerto/ets/GameObjectTest.java
+++ model/src/test/java/es/iespuerto/ets/GameObjectTest.java
@@ -1,8 +1,5 @@
 package es.iespuerto.ets;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.*;
-
 public class GameObjectTest {
     GameObject objeto1 = null;
     ID id = null;
